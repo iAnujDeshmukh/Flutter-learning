9e2708238a9bc727a375b3159ea7826fd3b1a3ce
Anuj Deshmukh <84030389+iAnujDeshmukh@users.noreply.github.com>
2024-12-10 11:23:15 +0530

    Add files via upload

---
+++ gradientBackgorund.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice/center_text.dart'; // Ensure this import is correct
+
+//variables
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientBackground extends StatelessWidget {
+  //Class Variables
+  // Color startGradientColour = Color.fromARGB(255, 26, 2, 80);
+  // Color endGradientColour = Color.fromARGB(255, 45, 7, 98);
+
+  //List approach
+  final List<Color> colours;
+
+  //default constructor
+  GradientBackground({super.key, required this.colours});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colours,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: CenterText("Hello Diacto"),
+      ),
+    );
+  }
+}
